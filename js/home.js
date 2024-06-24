9e30e0e6d438b2fb6f0218f29c9814fd1e588d4a
kena006 <82118161+kena006@users.noreply.github.com>
2024-06-23 20:53:04 -0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,77 @@
+document.addEventListener("DOMContentLoaded", function(event) {
 
+async function waitForElm(q) {
+	while (document.querySelector(q) == null) {
+		await new Promise(r => requestAnimationFrame(r));
+	};
+	return document.querySelector(q);
+};
+
+// Get user data
+
+const userData = document.getElementsByTagName("meta")[8];
+const username = userData.getAttribute("data-name");
+const userId = userData.getAttribute("data-userid");
+const under13 = userData.getAttribute("data-isunder13");
+
+const getUserImage = (type, id, size, format, circular) => {
+	return fetch("https://thumbnails.roblox.com/v1/users/" + type + "?userIds=" + id + "&size=" + size + "x" + size + "&format=" + format + "&isCircular=" + circular, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+    .then(res => {
+		return res.json();
+    })
+    .then(data => {
+		return data.data[0]["imageUrl"];
+    });
+};
+
+// Old style home page
+
+// Bring back home page greeting
+function header(link, image, name) {
+	return `
+	<div id="home-header" class="col-xs-12 home-header-container">
+		<div class="home-header">
+			<a href="${ link ?? "/users/1/profile" }" class="user-avatar-container avatar avatar-headshot-lg">
+				<span class="thumbnail-2d-container avatar-card-image">
+					<img src="${ image ?? "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" }" alt="" class="avatar-card-image">
+				</span>
+			</a>
+			<div class="user-info-container">
+				<h1 class="user-name-container">
+					<a href="${ link ?? "/users/1/profile" }">${ name ?? "Roblox" }</a>
+				</h1>
+			</div>
+		</div>
+	</div>
+	`;
+};
+
+waitForElm("#HomeContainer .section:first-child").then(async (hdrSec) => {
+	getUserImage("avatar-headshot", userId, "150", "Png", "false").then (async (image) => {
+		let link = "https://www.roblox.com/users/" + userId + "/profile";
+		let greetingText = "Hello, " + username + "!";
+		let htmlToInsert = header(link, image, greetingText);
+
+		hdrSec.outerHTML = htmlToInsert;
+	});
+});
+
+// Delete new homepage bloat
+// Wait for the friends list to be loaded in first to ensure the homepage is completely loaded
+
+waitForElm("#place-list > div > div > div.friend-carousel-container").then(async (friends) => {
+	var container = friends.parentNode
+	
+	for (let i = 0; i < 3; i++) {
+		container.childNodes[1].remove();
+	}
+	container.childNodes[5].remove();
+	container.childNodes[7].remove();
+});
+
+});
