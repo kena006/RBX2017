abc1fa00addb7064c98548902f04b1adc4551d37
kena006 <82118161+kena006@users.noreply.github.com>
2024-06-23 23:58:11 -0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -81,11 +81,11 @@ waitForElm("#place-list > div > div > div.friend-carousel-container").then(async
 	var container = friends.parentNode
 	
 	// Delete new homepage bloat
-	for (let i = 0; i < 3; i++) {
-		container.childNodes[1].remove();
-	}
-	container.childNodes[5].remove();
-	container.childNodes[7].remove();
+	//for (let i = 0; i < 3; i++) {
+	//	container.childNodes[1].remove();
+	//}
+	//container.childNodes[5].remove();
+	//container.childNodes[7].remove();
 	
 	// Game card player count
 	var gameCards = document.querySelectorAll(".grid-item-container.game-card-container")
